3bcfb3e7e00dc855bda458d341862acf599bce49
RNDLSMN <randolesmana@gmail.com>
2025-10-20 08:24:13 +0700

    Fix next.config.js: Properly compose all plugin configurations

--- next.config.js
+++ next.config.js
@@ -2,33 +2,26 @@ const withCSS = require("@zeit/next-css");
 const withImages = require("next-images");
 const nextTranslate = require("next-translate");
 
-module.exports = {
-  reactStrictMode: true,
-};
-module.exports = withCSS({
-  cssLoaderOptions: {
-    url: false,
-  },
-});
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        net: "empty",
-        tls: "empty",
-        dns: "empty",
-      };
-    }
-
-    return config;
-  },
-};
-module.exports = withImages();
-module.exports = nextTranslate({
-  reactStrictMode: true,
-});
+module.exports = nextTranslate(
+  withImages(
+    withCSS({
+      reactStrictMode: true,
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+      cssLoaderOptions: {
+        url: false,
+      },
+      webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve = config.resolve || {};
+          config.resolve.fallback = config.resolve.fallback || {};
+          config.resolve.fallback.net = false;
+          config.resolve.fallback.tls = false;
+          config.resolve.fallback.dns = false;
+        }
+        return config;
+      },
+    })
+  )
+);
